d675e68cf04790e42b0cfb2b60e269aad441bf2d
Alin Posorovaschi <alin.posorovaschi@e-spres-oh.com>
2016-03-09 18:01:44 +0200

    Replace Array with immutable Seq for holding the Node's state
     - It makes more sense since we are sending it in the results and converting from Array to Seq and back.

--- counter/src/main/scala/crdt/sample/counter/Node.scala
+++ counter/src/main/scala/crdt/sample/counter/Node.scala
@@ -1,29 +1,27 @@
 package crdt.sample.counter
 
-import akka.actor.{ActorRef, Actor, ActorLogging}
-import crdt.sample.{Siblings, Merge, Get, Inc}
+import akka.actor.{Actor, ActorLogging, ActorRef}
+import crdt.sample.{Get, Inc, Merge, Siblings}
 
 class Node extends Actor with ActorLogging {
 
-  val state = new Array[Int](VectorSize)
+  var state: Seq[Int] = (0 until VectorSize) map (_ => 0)
   var siblings = Seq.empty[ActorRef]
 
   def receive = {
     case Inc(pos) =>
-      state(pos) += 1
-      siblings foreach (actor => actor ! Merge(state.toSeq))
+      state = state.updated(pos, state(pos) + 1)
+      siblings foreach (actor => actor ! Merge(state))
       log.info(s"Received Inc($pos). New state=$printState")
     case Get => sender ! state.toVector
     case Merge(seq) =>
       log.info(s"Received merge request with content=$seq")
-      for (i <- state.indices) {
-        state(i) = Math.max(state(i), seq(i))
-      }
+      state = for ((s, news) <- state.zip(seq)) yield Math.max(s, news)
       log.info(s"State after merge=$printState")
     case Siblings(newSiblings) =>
       log.info(s"Received new siblings list $newSiblings")
       siblings = newSiblings
-      siblings foreach(a => a ! Merge(state.toSeq))
+      siblings foreach (a => a ! Merge(state))
   }
 
   def printState = state.mkString("(", ",", ")")
